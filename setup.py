fcf4d6602991f29b564a8392acaa87f6bc9d953f
Dalton Fury <daltonfury42@disroot.org>
2019-04-27 16:44:00 +0530

    Initial package structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="truecase",
+    version="0.0.1",
+    author="Dalton Fury",
+    author_email="daltonfury42@disroot.org",
+    description="A library to restore capitalization for text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daltonfury42/truecase",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0-or-later",
+        "Operating System :: OS Independent",
+    ],
+)
