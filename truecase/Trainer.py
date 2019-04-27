d3328555d26be0e347cd750268b61b6c6a0c6ce6
Dalton Fury <daltonfury42@disroot.org>
2019-04-27 22:02:40 +0530

    Added Trainer and Evaluator

---
+++ truecase/Trainer.py
@@ -0,0 +1,115 @@
+import nltk
+import pickle
+
+class Trainer(object):
+
+    def __init__(self):
+        self.uni_dist = None
+        self.backward_bi_dist = None
+        self.forward_bi_dist = None
+        self.trigram_dist = None
+        self.word_casing_lookup = None
+
+    def init(self):
+        self.uni_dist = nltk.FreqDist()
+        self.backward_bi_dist = nltk.FreqDist()
+        self.forward_bi_dist = nltk.FreqDist()
+        self.trigram_dist = nltk.FreqDist()
+        self.word_casing_lookup = {}
+
+    def train(self, corpus):
+
+        self.init()
+
+        # :: Create unigram lookup ::
+        for sentence in corpus:
+            if not self.check_sentence_sanity(sentence):
+                continue
+
+            for word_idx, word in enumerate(sentence):
+                self.uni_dist[word] += 1
+                word_lower = word.lower()
+                if word_lower not in self.word_casing_lookup:
+                    self.word_casing_lookup[word_lower] = set()
+
+                self.word_casing_lookup[word_lower].add(word)
+
+                try:
+                    if word_lower in self.word_casing_lookup and len(self.word_casing_lookup[word_lower]) >= 2:
+                        # Only if there are multiple options
+                        prev_word = sentence[word_idx - 1]
+
+                        self.backward_bi_dist[prev_word + "_" + word] += 1
+
+                        next_word = sentence[word_idx + 1].lower()
+                        self.forward_bi_dist[word + "_" + next_word] += 1
+                except IndexError:
+                    pass
+
+                try:
+                    if word_idx - 1 < 0:
+                        continue
+
+                    prev_word = sentence[word_idx - 1]
+                    cur_word = sentence[word_idx]
+                    cur_word_lower = word.lower()
+                    next_word_lower = sentence[word_idx + 1].lower()
+
+                    if cur_word_lower in self.word_casing_lookup and len(self.word_casing_lookup[cur_word_lower]) >= 2:
+                        # Only if there are multiple options
+                        self.trigram_dist[prev_word + "_" + cur_word + "_" + next_word_lower] += 1
+                except IndexError:
+                    pass
+
+    def save_to_file(self, file_path):
+        pickle_dict = {
+            'uni_dist': self.uni_dist,
+            'backward_bi_dist': self.backward_bi_dist,
+            'forward_bi_dist': self.forward_bi_dist,
+            'trigram_dist': self.trigram_dist,
+            'word_casing_lookup': self.word_casing_lookup,
+        }
+
+        with open(file_path, "wb") as fp:
+            pickle.dump(pickle_dict, fp)
+
+        print('Model saved to ' + file_path)
+
+    def get_casing(self, word):
+        """ Returns the casing of a word"""
+        if len(word) == 0:
+            return 'other'
+        elif word.isdigit():  # Is a digit
+            return 'numeric'
+        elif word.islower():  # All lower case
+            return 'allLower'
+        elif word.isupper():  # All upper case
+            return 'allUpper'
+        elif word[0].isupper():  # is a title, initial char upper, then all lower
+            return 'initialUpper'
+
+        return 'other'
+
+    def check_sentence_sanity(self, sentence):
+        """ Checks the sanity of the sentence. If the sentence is for example all uppercase, it is recjected"""
+        case_dist = nltk.FreqDist()
+
+        for token in sentence:
+            case_dist[self.get_casing(token)] += 1
+
+        if case_dist.most_common(1)[0][0] != 'allLower':
+            return False
+
+        return True
+
+if __name__ == '__main__':
+    corpus = nltk.corpus.brown.sents()  \
+             + nltk.corpus.reuters.sents() \
+             + nltk.corpus.semcor.sents() \
+             + nltk.corpus.conll2000.sents() \
+             + nltk.corpus.state_union.sents()
+
+    trainer = Trainer()
+    trainer.train(corpus)
+
+    trainer.save_to_file('data/english.dist')
---
+++ truecase/TrueCaser.py
@@ -0,0 +1,113 @@
+import pickle
+
+import math
+import nltk
+
+
+class TrueCaser(object):
+    def __init__(self, dist_file_path):
+        with open('english_distributions.obj', 'rb') as distributions_file:
+            pickle_dict = pickle.load(distributions_file)
+            self.uni_dist = pickle_dict['uni_dist']
+            self.backward_bi_dist = pickle_dict['backward_bi_dist']
+            self.forward_bi_dist = pickle_dict['forward_bi_dist']
+            self.trigram_dist = pickle_dict['trigram_dist']
+            self.word_casing_lookup = pickle_dict['word_casing_lookup']
+
+    def get_score(self, prev_token, possible_token, next_token):
+        pseudo_count = 5.0
+
+        # Get Unigram Score
+        nominator = self.uni_dist[possible_token] + pseudo_count
+        denominator = 0
+        for alternativeToken in self.word_casing_lookup[possible_token.lower()]:
+            denominator += self.uni_dist[alternativeToken] + pseudo_count
+
+        unigram_score = nominator / denominator
+
+        # Get Backward Score
+        bigram_backward_score = 1
+        if prev_token is not None:
+            nominator = self.backward_bi_dist[prev_token + '_' + possible_token] + pseudo_count
+            denominator = 0
+            for alternativeToken in self.word_casing_lookup[possible_token.lower()]:
+                denominator += self.backward_bi_dist[prev_token + '_' + alternativeToken] + pseudo_count
+
+            bigram_backward_score = nominator / denominator
+
+        # Get Forward Score
+        bigram_forward_score = 1
+        if next_token is not None:
+            next_token = next_token.lower()  # Ensure it is lower case
+            nominator = self.forward_bi_dist[possible_token + "_" + next_token] + pseudo_count
+            denominator = 0
+            for alternativeToken in self.word_casing_lookup[possible_token.lower()]:
+                denominator += self.forward_bi_dist[alternativeToken + "_" + next_token] + pseudo_count
+
+            bigram_forward_score = nominator / denominator
+
+        # Get Trigram Score
+        trigram_score = 1
+        if prev_token is not None and next_token != None:
+            next_token = next_token.lower()  # Ensure it is lower case
+            nominator = self.trigram_dist[prev_token + "_" + possible_token + "_" + next_token] + pseudo_count
+            denominator = 0
+            for alternativeToken in self.word_casing_lookup[possible_token.lower()]:
+                denominator += self.trigram_dist[prev_token + "_" + alternativeToken + "_" + next_token] + pseudo_count
+
+            trigram_score = nominator / denominator
+
+        result = math.log(unigram_score) + math.log(bigram_backward_score) + math.log(bigram_forward_score) + math.log(
+            trigram_score)
+
+        return result
+
+    def get_true_case(self, sentence, out_of_vocabulary_token_option):
+        """
+        Returns the true case for the passed tokens.
+        @param tokens: Tokens in a single sentence
+        @param outOfVocabulariyTokenOption:
+            title: Returns out of vocabulary (OOV) tokens in 'title' format
+            lower: Returns OOV tokens in lower case
+            as-is: Returns OOV tokens as is
+        """
+        tokens = nltk.word_tokenize(sentence)
+
+        tokens_true_case = []
+        for token_idx, token in enumerate(tokens):
+
+            if token in nltk.string.punctuation or token.isdigit():
+                tokens_true_case.append(token)
+            else:
+                if token in self.word_casing_lookup:
+                    if len(self.word_casing_lookup[token]) == 1:
+                        tokens_true_case.append(list(self.word_casing_lookup[token])[0])
+                    else:
+                        prev_token = tokens_true_case[token_idx - 1] if token_idx > 0 else None
+                        next_token = tokens[token_idx + 1] if token_idx < len(tokens) - 1 else None
+
+                        best_token = None
+                        highest_score = float("-inf")
+
+                        for possible_token in self.word_casing_lookup[token]:
+                            score = self.get_score(prev_token, possible_token, next_token)
+
+                            if score > highest_score:
+                                best_token = possible_token
+                                highest_score = score
+
+                        tokens_true_case.append(best_token)
+
+                    if token_idx == 0:
+                        tokens_true_case[0] = tokens_true_case[0].title();
+
+                else:  # Token out of vocabulary
+                    if out_of_vocabulary_token_option == 'title':
+                        tokens_true_case.append(token.title())
+                    elif out_of_vocabulary_token_option == 'lower':
+                        tokens_true_case.append(token.lower())
+                    else:
+                        tokens_true_case.append(token)
+
+        return tokens_true_case
+
