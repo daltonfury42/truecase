9d52cf02b17814002f6f929b6f02aa7eb55dcf22
Louis Martin <louismartincs@gmail.com>
2019-12-23 05:30:30 +0100

    Lazy load the TrueCaser() in __init__.py for faster import (-1s) (#4)

--- truecase/__init__.py
+++ truecase/__init__.py
@@ -1,8 +1,12 @@
+from functools import lru_cache
+
 from .TrueCaser import TrueCaser
 
-caser = TrueCaser()
+
+@lru_cache(maxsize=1)
+def get_truecaser():
+    return TrueCaser()
+
 
 def get_true_case(sentence, out_of_vocabulary_token_option='title'):
-
-    return caser.get_true_case(sentence, out_of_vocabulary_token_option=out_of_vocabulary_token_option)
-
+    return get_truecaser().get_true_case(sentence, out_of_vocabulary_token_option=out_of_vocabulary_token_option)
