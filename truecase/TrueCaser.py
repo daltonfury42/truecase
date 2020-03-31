ae692969b183f50ddff4918d6b0bb6a10e96d980
Sergi Alonso <sergi.alonsobdia@innovationalpha.es>
2020-03-31 12:31:37 +0200

    :tophat: add fix for upper cases contractions

--- truecase/TrueCaser.py
+++ truecase/TrueCaser.py
@@ -4,7 +4,6 @@ import pickle
 import string
 
 import nltk
-from nltk.tokenize import TweetTokenizer
 
 
 class TrueCaser(object):
@@ -22,7 +21,7 @@ class TrueCaser(object):
             self.forward_bi_dist = pickle_dict["forward_bi_dist"]
             self.trigram_dist = pickle_dict["trigram_dist"]
             self.word_casing_lookup = pickle_dict["word_casing_lookup"]
-        self.tknzr = TweetTokenizer()
+        self.tknzr = nltk.TweetTokenizer()
 
     def get_score(self, prev_token, possible_token, next_token):
         pseudo_count = 5.0
@@ -30,9 +29,9 @@ class TrueCaser(object):
         # Get Unigram Score
         nominator = self.uni_dist[possible_token] + pseudo_count
         denominator = 0
-        for alternativeToken in self.word_casing_lookup[
+        for alternative_token in self.word_casing_lookup[
                 possible_token.lower()]:
-            denominator += self.uni_dist[alternativeToken] + pseudo_count
+            denominator += self.uni_dist[alternative_token] + pseudo_count
 
         unigram_score = nominator / denominator
 
@@ -43,10 +42,10 @@ class TrueCaser(object):
                 self.backward_bi_dist[prev_token + "_" + possible_token] +
                 pseudo_count)
             denominator = 0
-            for alternativeToken in self.word_casing_lookup[
+            for alternative_token in self.word_casing_lookup[
                     possible_token.lower()]:
                 denominator += (self.backward_bi_dist[prev_token + "_" +
-                                                      alternativeToken] +
+                                                      alternative_token] +
                                 pseudo_count)
 
             bigram_backward_score = nominator / denominator
@@ -59,10 +58,10 @@ class TrueCaser(object):
                 self.forward_bi_dist[possible_token + "_" + next_token] +
                 pseudo_count)
             denominator = 0
-            for alternativeToken in self.word_casing_lookup[
+            for altertantive_token in self.word_casing_lookup[
                     possible_token.lower()]:
                 denominator += (
-                    self.forward_bi_dist[alternativeToken + "_" + next_token] +
+                    self.forward_bi_dist[alternative_token + "_" + next_token] +
                     pseudo_count)
 
             bigram_forward_score = nominator / denominator
@@ -74,10 +73,10 @@ class TrueCaser(object):
             nominator = (self.trigram_dist[prev_token + "_" + possible_token +
                                            "_" + next_token] + pseudo_count)
             denominator = 0
-            for alternativeToken in self.word_casing_lookup[
+            for alternative_token in self.word_casing_lookup[
                     possible_token.lower()]:
                 denominator += (
-                    self.trigram_dist[prev_token + "_" + alternativeToken +
+                    self.trigram_dist[prev_token + "_" + alternative_token +
                                       "_" + next_token] + pseudo_count)
 
             trigram_score = nominator / denominator
@@ -91,7 +90,7 @@ class TrueCaser(object):
         fix_sentence = list(sentence)
         previous_char = ''
         for idx, char in enumerate(fix_sentence):
-            if char.isupper() and previous_char is "'":
+            if char.isupper() and previous_char == "'":
                 fix_sentence[idx] = char.lower()
             previous_char = char
 
