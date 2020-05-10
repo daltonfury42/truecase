ff0e3920c92118588cdee813f5e7c3eff75f5898
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-05-10 16:16:33 +0530

    Fix setup.py (#14)
    * Fix License
    
    * pip3 in readme
    
    * update gitignore
    
    * fix build issue
    
    Co-authored-by: Navaneeth Kishore <navaneeth@spotnana.com>

--- setup.py
+++ setup.py
@@ -21,12 +21,12 @@ setuptools.setup(
     name="truecase",
     version=truecase.__version__,
     author="Dalton Fury",
-    author_email="daltonfury42@disroot.org",
+    author_email="daltonfury42 <at> disroot <dot> org",
     description="A library to restore capitalization for text",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/daltonfury42/truecase",
-    packages=setuptools.find_packages(where="src", exclude=["tests"]),
+    packages=setuptools.find_packages(exclude=["tests"]),
     setup_requires=["pytest-runner"],
     tests_require=get_requirements("requirements.test.txt"),
     classifiers=[
@@ -34,6 +34,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    license='MIT',
     install_requires=get_requirements(),
     include_package_data=True,
 )
